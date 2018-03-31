574c90f60af3f3c0ca50bb3158dc83edad4abcd6
Jose Medina <fermed28@gmail.com>
2018-03-30 22:22:51 -0500

    add handle events: onChange to input and onSubmit to form. add Two states value: input value to connect an control the value parameter and list to concat element

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      value : "",
+      list : []
+    }
+    this.hadlechange= this.hadlechange.bind(this)
+    this.handleSubmit= this.handleSubmit.bind(this)    
+  }
+
+  hadlechange(e){
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    const list= this.state.list
+    this.setState({list: list.concat(this.state.value),
+                    value: ""}) 
+  
+  }
   render() {
     return (
       <div className="wrapper">
@@ -12,9 +35,10 @@ class App extends Component {
             <li>Sacar la ropa</li>
             <li>Hacer la cama</li>
             <li>Leer un rato</li>
+            {this.state.list.map (lis=> <li>{lis}</li>)}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onSubmit= {this.handleSubmit}>
+             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.value} onChange= {this.hadlechange} />
            </form>
         </div>
       </div>
